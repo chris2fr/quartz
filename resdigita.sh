3653b0dcb94c04ac573be233ef147e43161a873f
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-27 11:34:11 +0100

    doc.resdigita.com

--- resdigita.sh
+++ resdigita.sh
@@ -1,6 +1,6 @@
 cd /mnt/d/work/quartz/
 rm ./assets/indices/*.json
 hugo-obsidian -input=resdigita -output=assets/indices -index -root=.
-hugo --enableGitInfo --minify --baseURL=https://doc.lesgrandsvoisins.com
+hugo --enableGitInfo --minify --baseURL=https://doc.resdigita.com
 cp -a ./assets/fonts/ ./public/fonts
 rsync -a --delete public/ www.resdigita.org:/var/www/resdigitacom
